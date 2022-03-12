5f906f667eba6796181a4c515cb32d7b87eb6456
nauval123 <nayuya24@gmail.com>
2022-03-12 14:48:11 +0700

    s

--- lib/pages/3dmodelpage/testar.dart
+++ lib/pages/3dmodelpage/testar.dart
@@ -5,7 +5,7 @@ import 'package:modul_pembelajaran_kimia/model/Molecule.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARPage extends StatefulWidget {
-  const ARPage({ Key key }) : super(key: key);
+  const ARPage({Key key}) : super(key: key);
 
   @override
   _ARPageState createState() => _ARPageState();
@@ -13,12 +13,12 @@ class ARPage extends StatefulWidget {
 
 class _ARPageState extends State<ARPage> {
   ArCoreController arCoreController;
- Molecule modelofmolecule;
+  Molecule modelofmolecule;
 
- @override
+  @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero,(){
+    Future.delayed(Duration.zero, () {
       setState(() {
         modelofmolecule = ModalRoute.of(context).settings.arguments as Molecule;
       });
@@ -27,7 +27,8 @@ class _ARPageState extends State<ARPage> {
 
   @override
   Widget build(BuildContext context) {
-    final informationofmolecule = ModalRoute.of(context).settings.arguments as Molecule;
+    final informationofmolecule =
+        ModalRoute.of(context).settings.arguments as Molecule;
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
@@ -43,41 +44,36 @@ class _ARPageState extends State<ARPage> {
     );
   }
 
-
-  
-
   void _onArCoreViewCreated(ArCoreController controller) {
-    
     arCoreController = controller;
     // arCoreController.onPlaneDetected=;
     // arCoreController.onPlaneTap=_onHandleTap;
     // arCoreController.onPlaneDetected=;
     _modelMolecule(arCoreController);
     _addCube(arCoreController);
-    
   }
 
   // void _onHandleTap(List<ArCoreHitTestResult> hits){
   //   final hit = hits.first;
   //   _modelMolecule(hit);
-  // } 
+  // }
 
-    void _modelMolecule (ArCoreController controller){
+  void _modelMolecule(ArCoreController controller) {
     final model = ArCoreReferenceNode(
-      scale: vector.Vector3(0.8,0.8,0.8),
+      scale: vector.Vector3(0.8, 0.8, 0.8),
       object3DFileName: modelofmolecule.modelar,
       position: vector.Vector3(0.0, 0.0, -0.35),
-      rotation:vector.Vector4(0, 0, 0, 0),
+      rotation: vector.Vector4(0, 0, 0, 0),
       // position:  plane.pose.translation+vector.Vector3(0.0,0.0,0.0),
       // rotation: plane.pose.rotation,
-      );
-      
-      controller.addArCoreNode(model);
-      // arCoreController.addArCoreNode(model);
-      // arCoreController.onTrackingImage;
+    );
+
+    controller.addArCoreNode(model);
+    // arCoreController.addArCoreNode(model);
+    // arCoreController.onTrackingImage;
   }
 
-    void _addCube(ArCoreController controller) {
+  void _addCube(ArCoreController controller) {
     final material = ArCoreMaterial(
       color: Color.fromARGB(120, 66, 134, 244),
       metallic: 1.0,
@@ -92,12 +88,11 @@ class _ARPageState extends State<ARPage> {
     );
     controller.addArCoreNode(node);
   }
-  
-  
+
   @override
   void dispose() {
-    arCoreController.dispose();
     super.dispose();
+
+    arCoreController.dispose();
   }
 }
-
