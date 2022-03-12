f3ea7cc30952554eb5a437e35ae05a2e56a8d22d
nauval123 <nayuya24@gmail.com>
2022-03-13 03:28:13 +0700

    s

--- lib/pages/3dmodelpage/3DModelAtom.dart
+++ lib/pages/3dmodelpage/3DModelAtom.dart
@@ -27,47 +27,6 @@ class ModelAtomPage extends StatelessWidget {
                 cameraControls: true,
                 // ar:true,
               ),
-              Spacer(),
-              DraggableScrollableSheet(
-                initialChildSize: 0.05,
-                minChildSize: 0.05,
-                maxChildSize: 0.5,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(25),
-                          topRight: Radius.circular(25)),
-                      color: Colors.white,
-                    ),
-                    margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 20, bottom: 30),
-                            height: 10,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(25),
-                              color: Colors.blue[300],
-                            ),
-                          ),
-                          Center(
-                            child: Text(informationofmolecule.moleculename),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: Text(informationofmolecule.description),
-                          )
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              )
             ],
           )),
     );
