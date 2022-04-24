04840285bfbef637c551094245d66853b1ae785c
nauval123 <nayuya24@gmail.com>
2022-04-24 14:43:06 +0700

    revisi 24 april 2022

--- lib/pages/TujuanPembelajaranPage.dart
+++ lib/pages/TujuanPembelajaranPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class TujuanPembelajaranPage extends StatelessWidget {
   const TujuanPembelajaranPage({Key? key}) : super(key: key);
@@ -17,13 +18,17 @@ class TujuanPembelajaranPage extends StatelessWidget {
           child: Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                    "assets/Tujuan Pembelajaran/Tujuan Pembelajaran.png"),
-              ),
+            child: PhotoView(
+              imageProvider: AssetImage(
+                  "assets/Tujuan Pembelajaran/Tujuan Pembelajaran.png"),
             ),
+            // decoration: BoxDecoration(
+            //   image: DecorationImage(
+            //     fit: BoxFit.fill,
+            //     image: AssetImage(
+            //         "assets/Tujuan Pembelajaran/Tujuan Pembelajaran.png"),
+            //   ),
+            // ),
           ),
         ),
       ),
